5c6390fb89f530decb133aedda5e21021fc62f18
Ushmani <ushmani.garg@gmail.com>
2023-07-23 19:50:25 +0530

    Added

---
+++ src/components/CounterComponent.js
@@ -0,0 +1,35 @@
+// CounterComponent.js
+import React, { Component } from 'react';
+
+class CounterComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter: {this.state.count}</h1>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterComponent;
